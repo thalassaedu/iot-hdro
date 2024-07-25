1a0bdbd97b37e8bf946c293024d154b52b01a5b5
srecops <nitin.rana@srecops.com>
2024-07-25 21:54:27 +0200

    added files

--- platform/python/apiv5.py
+++ platform/python/apiv5.py
@@ -1,24 +1,24 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
-# Store raw Arduino data in memory
-raw_arduino_data = []
+# Store raw data in memory
+received_data = []
 
-@app.route('/update_arduino', methods=['POST'])
-def update_arduino():
+@app.route('/update_data', methods=['POST'])
+def update_data():
     data = request.data.decode('utf-8')
     if not data:
-        return jsonify({'error': 'Invalid data format'}), 400
+        return {'error': 'Invalid data format'}, 400
 
     # Append raw data to the in-memory list
-    raw_arduino_data.append(data)
-    print("Received raw data:", data)
-    return jsonify({'status': 'success'}), 200
+    received_data.append(data)
+    print("Received data:", data)
+    return {'status': 'success'}, 200
 
-@app.route('/arduino-data', methods=['GET'])
-def get_arduino_data():
-    return '<br>'.join(raw_arduino_data), 200
+@app.route('/data', methods=['GET'])
+def get_data():
+    return '<br>'.join(received_data), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8081)
