bdae564c6ba91d64d2ebb898237d447234b9bab8
srecops <nitin.rana@srecops.com>
2024-10-26 16:55:31 +0200

    added files

--- agro-hydro-v3/sensors/ph.py
+++ agro-hydro-v3/sensors/ph.py
@@ -1,15 +1,10 @@
 from machine import Pin
 from time import sleep
 
-# Configure pin 32 as input
-probe_pin = Pin(32, Pin.IN)
+# Change the pin to GPIO 27 (or another from the list)
+probe_pin = Pin(27, Pin.IN)
 
 while True:
-    # Read the digital value (0 or 1)
     probe_value = probe_pin.value()
-    
-    # Print the value to the console
     print("Probe Value:", probe_value)
-    
-    # Wait for a short period
-    sleep(0.5)  # Adjust delay as needed
+    sleep(0.5)
